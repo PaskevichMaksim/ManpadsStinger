15af7927e3df6fe5ce95edc3ea49d33ad37cd5c9
PaskevichMaksim <paskevichmaks@gmail.com>
2024-06-24 11:47:43 +0300

    Add motion

---
+++ Assets/Scripts/Movement/TeleportActivator.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class TeleportActivator : MonoBehaviour
+{
+  private const float DEVIATION_VALUE = .5f;
+  
+   [SerializeField]
+   private XRRayInteractor _rightRay;
+   [SerializeField]
+   private XRRayInteractor _leftRay;
+   [SerializeField]
+   private InputActionProperty _rightAction;
+   [SerializeField]
+   private InputActionProperty _leftAction;
+
+   private void Update()
+   {
+      _rightRay.gameObject.SetActive(ToggleRay(_rightAction));
+      _leftRay.gameObject.SetActive(ToggleRay(_leftAction));
+   }
+
+   public void ChangeActiveXRController(GameObject controller)
+   {
+     controller.SetActive(!controller.activeSelf);
+   }
+
+   private bool ToggleRay (InputActionProperty inputAction)
+   {
+     return inputAction.action.ReadValue<Vector2>().y > DEVIATION_VALUE;
+   }
+   
+}
